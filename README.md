9c14910741c8112de1649a1837686335fa24d88b
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-05-10 14:53:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,128 @@
+# Graphql Global Id Directive
+
+While transitioning to the use of global IDs (commonly known as node IDs), I found that encoding and decoding these IDs was a necessary step in various parts of my application, especially when using database IDs for queries and mutations. I sought a way to simplify this process by decoding the IDs before they reached the resolvers and encoding them on their way out. To address this, I created a package that makes the use of global IDs straightforward and efficient. This package includes a `GlobalIdDirective` class, which offers two directives: `globalIdEncodeDirective` and `globalIdDecodeDirective`. These directives handle the encoding and decoding of global IDs, making the integration of global IDs into any GraphQL application a breeze.
+
+## Installation
+
+```bash
+npm install graphql-global-id-directive
+```
+
+## Usage
+
+First, import and create an instance of the `GlobalIdDirective` class:
+
+```JavaScript
+import { GlobalIdDirective } from "graphql-global-id-directive";
+const globalIdDirective = new GlobalIdDirective();
+```
+
+### Define your schema
+
+```js
+const typeDefs = `
+  interface Node {
+    id: ID!
+  }
+
+  type Query {
+    user(id: ID! @decodeGlobalId): User
+    node(id: ID! @globalIdDecode(returnIdOnly: false)): Node
+  }
+
+  type User implements Node {
+    id: ID! @encodeGlobalId(typeName: "User")
+    name: String
+    email: String
+  }
+`;
+```
+
+In the type definition of the node query, use the `@globalIdDecode` directive with the `returnIdOnly` option set to `false`. This ensures that the stringified decoded global ID object is returned instead of just the underlying id which is the default behavior, allowing you to parse and use both the underlying `id` and `__typename` in your resolver.
+
+### Apply the directives to your schema
+
+```js
+const { globalIdEncodeDirectiveTypeDefs, globalIdEncodeDirectiveTransformer } =
+  globalIdDirective.globalIdEncodeDirective("encodeGlobalId"); // Use any name of your choice but avoid collisions with other directive names
+
+const { globalIdDecodeDirectiveTypeDefs, globalIdDecodeDirectiveTransformer } =
+  globalIdDirective.globalIdDecodeDirective("decodeGlobalId");
+
+let schema = makeExecutableSchema({
+  typeDefs: [
+    typeDefs,
+    globalIdEncodeDirectiveTypeDefs,
+    globalIdDecodeDirectiveTypeDefs,
+  ],
+});
+
+const directiveTransformers = [
+  globalIdEncodeDirectiveTransformer,
+  globalIdDecodeDirectiveTransformer,
+];
+
+schema = directiveTransformers.reduce(
+  (currentSchema, transformer) => transformer(currentSchema),
+  schema
+);
+```
+
+## Using GlobalIdDirective with Node Resolver
+
+You can utilize the decoded global IDs in your node resolver to retrieve any instance of a node. Here's an example of how you can do this:
+
+```javascript
+node: async (_, args, { dataSources }) => {
+  // Parse the stringified ID back into an object
+  const { id, __typename } = JSON.parse(args.id.toString());
+
+  let node;
+  switch (__typename) {
+    case "User":
+      node = await dataSources.userDb.getUser({ userId: id }); // Or fetch the user directly with the model. EG: UserModel.findById(id)
+      break;
+    case "Product":
+      node = await dataSources.productDb.getProduct({ productId: id });
+      break;
+    case "Order":
+      node = await dataSources.brandDb.getOrder({ orderId: id });
+      break;
+    // Add more cases as needed for other typenames
+    default:
+      throw new Error(`Unsupported typename: ${__typename}`); //Or do  perform another action
+  }
+
+  return { ...node.toObject({ virtuals: true }), __typename }; // I used a Mongoose document for this example
+};
+```
+
+## Customization
+
+The `GlobalIdDirective` class is designed to be easily customizable. You can extend the class and override the `encodeFn` and `decodeFn` methods to provide your own implementation for encoding and decoding global IDs.
+
+Here's an example of how you can do this:
+
+```javascript
+class CustomGlobalIdDirective extends GlobalIdDirective {
+  encodeFn(id, typeName) {
+    // Your custom encoding logic here
+  }
+
+  decodeFn(globalId) {
+    // Your custom decoding logic here
+  }
+}
+
+const customGlobalIdDirective = new CustomGlobalIdDirective();
+```
+
+In this example, CustomGlobalIdDirective is a subclass of GlobalIdDirective. It overrides the encodeFn and decodeFn methods with custom logic for encoding and decoding global IDs.
+
+## Contributing
+
+Want to make this package even better? Fork the repository and submit a pull request to contribute.
+
+## License
+
+MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/GlobalIdDirective.ts
@@ -0,0 +1,158 @@
+import { getDirective, MapperKind, mapSchema } from "@graphql-tools/utils";
+import {
+  defaultFieldResolver,
+  GraphQLScalarType,
+  GraphQLSchema,
+} from "graphql";
+
+export class GlobalIdDirective {
+  constructor() {}
+
+  encodeFn(id: string, typeName: string) {
+    return Buffer.from(`${id}:${typeName}`, "utf8")
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+  }
+
+  decodeFn(globalId: string) {
+    const paddedGlobalId = globalId + "===".slice((globalId.length + 3) % 4);
+    const urlSafeGlobalId = paddedGlobalId
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+    const decoded = Buffer.from(urlSafeGlobalId, "base64").toString("utf8");
+    const parts = decoded.split(":");
+
+    // Check if the decoded string has the correct format
+    if (parts.length !== 2) {
+      throw new Error(`Invalid global ID: ${globalId}`);
+    }
+
+    return { id: parts[0], __typename: parts[1] };
+  }
+
+  globalIdEncodeDirective(directiveName: string) {
+    const typeDirectiveArgumentMaps: Record<string, any> = {};
+
+    return {
+      globalIdEncodeDirectiveTypeDefs: `directive @${directiveName}(typeName: String!) on FIELD_DEFINITION`,
+      globalIdEncodeDirectiveTransformer: (schema: GraphQLSchema) =>
+        mapSchema(schema, {
+          [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
+            const globalIdEncodeDirective =
+              getDirective(schema, fieldConfig, directiveName)?.[0] ??
+              typeDirectiveArgumentMaps[typeName];
+            if (globalIdEncodeDirective) {
+              const { typeName } = globalIdEncodeDirective;
+              const { resolve = defaultFieldResolver } = fieldConfig;
+
+              fieldConfig.resolve = async (source, args, context, info) => {
+                const result = await resolve.call(
+                  this,
+                  source,
+                  args,
+                  context,
+                  info
+                );
+
+                //return this.encodeFn(result.toString(), typeName);
+
+                // Check if result is null or undefined before calling toString()
+                if (result != null) {
+                  return this.encodeFn(result.toString(), typeName);
+                } else {
+                  //throw new Error("Result is null or undefined.");
+                }
+              };
+              return fieldConfig;
+            }
+          },
+        }),
+    };
+  }
+
+  globalIdDecodeDirective(directiveName: string) {
+    const typeDirectiveArgumentMaps: Record<string, any> = {};
+
+    return {
+      globalIdDecodeDirectiveTypeDefs: `directive @${directiveName}(returnIdOnly: Boolean = true) on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION`,
+      globalIdDecodeDirectiveTransformer: (schema: GraphQLSchema) =>
+        mapSchema(schema, {
+          [MapperKind.OBJECT_FIELD]: (fieldConfig, fieldName, typeName) => {
+            const originalResolve = fieldConfig.resolve || defaultFieldResolver;
+            fieldConfig.resolve = (source, args, context, info) => {
+              if (fieldConfig.args) {
+                // Decode all arguments that have the directive
+                Object.keys(fieldConfig.args).forEach((argName) => {
+                  const argConfig = fieldConfig.args![argName];
+                  const globalIdDecodeDirective = getDirective(
+                    schema,
+                    argConfig,
+                    directiveName
+                  )?.[0];
+                  if (globalIdDecodeDirective) {
+                    const decodedObject = this.decodeFn(args[argName]);
+
+                    // args[argName] = globalIdDecode(args[argName]);
+
+                    // Use only the id from the decoded object if returnIdOnly is true
+                    args[argName] =
+                      globalIdDecodeDirective.returnIdOnly !== false
+                        ? decodedObject.id
+                        : JSON.stringify(decodedObject);
+                  }
+                });
+              }
+              return originalResolve.call(this, source, args, context, info);
+            };
+            return fieldConfig;
+          },
+
+          [MapperKind.INPUT_OBJECT_FIELD]: (
+            inputFieldConfig,
+            fieldName,
+            typeName
+          ) => {
+            const globalIdDecodeDirective =
+              getDirective(schema, inputFieldConfig, directiveName)?.[0] ??
+              typeDirectiveArgumentMaps[typeName];
+            if (globalIdDecodeDirective) {
+              if (inputFieldConfig.type instanceof GraphQLScalarType) {
+                const originalType = inputFieldConfig.type;
+                const self = this; // Capture 'this' reference
+                inputFieldConfig.type = new GraphQLScalarType({
+                  ...originalType.toConfig(),
+                  name: `Decodable${originalType.name}`,
+                  //description: `A  decoded ${originalType.name}`,
+                  //  serialize: originalType.serialize.bind(originalType),
+                  /*parseValue(value) {
+                  return originalType.parseValue.bind(originalType)(value);
+                },*/
+                  parseValue: (value: unknown) => {
+                    // Parse value with original parseValue method
+                    const parsedValue =
+                      originalType.parseValue.bind(originalType)(value);
+
+                    // Parse global ID to raw ID
+                    if (typeof parsedValue === "string") {
+                      try {
+                        const decodedObject = self.decodeFn(parsedValue);
+                        return globalIdDecodeDirective.returnIdOnly !== false
+                          ? decodedObject.id
+                          : JSON.stringify(decodedObject);
+                      } catch (err) {
+                        throw new Error("Invalid GlobalId");
+                      }
+                    }
+                    throw new Error("Expected string for parsing into ID");
+                  },
+                });
+              }
+              return inputFieldConfig;
+            }
+          },
+        }),
+    };
+  }
+}
